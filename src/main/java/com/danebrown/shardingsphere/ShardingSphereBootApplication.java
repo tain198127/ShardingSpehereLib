52ce885452596d2e4aac54238213fd572a8d1eac
dane brown <tain198127@163.com>
2021-07-26 11:14:20 +0800

    配置上sharding jdbc

---
+++ src/main/java/com/danebrown/shardingsphere/ShardingSphereBootApplication.java
@@ -0,0 +1,39 @@
+package com.danebrown.shardingsphere;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import javax.swing.*;
+
+/**
+ * Created by danebrown on 2021/7/26
+ * mail: tain198127@163.com
+ *
+ * @author danebrown
+ */
+@SpringBootApplication
+@Component
+@Log4j2
+public class ShardingSphereBootApplication implements CommandLineRunner {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ShardingSphereBootApplication.class).
+                web(WebApplicationType.NONE)
+                .run(args);
+    }
+
+    @Resource
+    DataSource dataSource;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("{}",dataSource);
+    }
+}
